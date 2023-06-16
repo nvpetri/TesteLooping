057f6f0e4db648c50f7329acaead123a9cf661b5
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2023-06-16 12:52:45 -0300

    first commit

---
+++ src/br/senai/sp/jandira/TesteLooping.java
@@ -0,0 +1,18 @@
+package br.senai.sp.jandira;
+
+public class TesteLooping {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+		int i = 100;
+		
+		while( i >= 1) {
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
